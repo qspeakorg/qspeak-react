b23cd6b46a2526a69a1aa787ee71d15f65857730
Angela He <angi.lucian@gmail.com>
2022-02-01 14:06:39 -0500

    adding image

--- qspeak-react/src/components/navbar.js
+++ qspeak-react/src/components/navbar.js
@@ -1,16 +1,16 @@
 import react from "react";
 import './navbar.css';
+import logo from "../assets/Black_Logo.png"
 
 function Navbar(){
     return(
         <div className="container">
-            <img src="http://www.qspeak.org/assets/img/Black%20Logo.png" className="logo"></img>
+            <img src={logo} className="logo"></img>
             <div className="nav">
                 <button className="button">ABOUT US</button>
                 <button className="button">TEAM</button>
                 <button className="button">BLOG</button>
                 <button className="button">CONTACT US</button>
-
             </div>
         </div>
     );
